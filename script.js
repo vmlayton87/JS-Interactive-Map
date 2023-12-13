961423f0d1cdaea7457dcc3e634771da7927d3f8
vmlayton87 <vmlayton87@gmail.com>
2023-12-13 12:32:45 -0500

    return business array from foursquare function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // comments
 
-// get users current location
+// users current location function
 
 async function getCoords (){
     let currentPos = await new Promise((resolve, reject) => {
@@ -13,32 +13,32 @@ async function getCoords (){
 
 //console.log(getCoords())  
 
-// create user location map
+// user location map object
 
-const currentLocationMap = {
+const myMap = {
     coordinates: [],
-
-    map: {},
+    displayMap: {},
 
     buildMap: function() {
-        this.map = L.map('userMap').setView([this.coordinates[0], this.coordinates[1]], 15);
+        this.displayMap = L.map('userMap').setView([this.coordinates[0], this.coordinates[1]], 15);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 20,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(this.map);
+        }).addTo(this.displayMap);
 
-        L.marker([this.coordinates[0], this.coordinates[1]]).addTo(this.map).bindPopup('<p1><b>Your Location</b></p1>').openPopup();
+        L.marker([this.coordinates[0], this.coordinates[1]]).addTo(this.displayMap).bindPopup('<p1><b>Your Location</b></p1>').openPopup();
     }
 }
 
 
 // makes current coordinates available to use in create map function/object and adds the map onto the page
+// creates a map in the div using the users current location
 window.onload = async () => {
     const coords = await getCoords()
-    currentLocationMap.coordinates = coords
-    currentLocationMap.buildMap()
-    //console.log(currentLocationMap)
+    myMap.coordinates = coords
+    myMap.buildMap()
+    //console.log(myMap)
 }
 
 // When user selects a category from the drop down menu, show 5 closest locations that match that category. 
@@ -48,7 +48,7 @@ window.onload = async () => {
     let selectCategoryElement = document.getElementById("location-category-select")
 
     // add event listener
-    document.getElementById("location-category-select").onchange = function() {placeSearch(this.value,currentLocationMap.coordinates)};
+   // document.getElementById("location-category-select").onchange = function() {placeSearch(this.value,currentLocationMap.coordinates)};
 
     // function for things to happen when category is selected
     function myFunction(val) {
@@ -57,34 +57,40 @@ window.onload = async () => {
     
    
     
-    //buildCategoryMap:
-    //const buildCategoryMap = {}
-    async function placeSearch(category,latLong) {
-        try {
-            const searchParams = new URLSearchParams({
-              query: category,
-              ll: latLong,
-              open_now: 'true',
-              sort: 'DISTANCE',
-              limit: 5
-            });
-            const results = await fetch(
-              `https://api.foursquare.com/v3/places/search?${searchParams}`,
-              {
-                method: 'GET',
-                headers: {
-                  Accept: 'application/json',
-                  Authorization: 'fsq31kaK6M0eHOYrzVjc66oAKgQ7sAg6pwkxvyqH+F3ixUU=',
-                }
-              }
-            );
-            const data = await results.json();
-            console.log(data)
-            return data;
-        } catch (err) {
-            console.error(err);
-        } 
-        
-    }
-    
+    // buildCategoryMap:
+
+        //grab the select element drop down menu and add event listener
+        selectCategoryElement = document.getElementById("location-category-select").onchange = function() {
+            placeSearch(this.value, myMap.coordinates)
+            console.log("data from the element select: " , placeSearch(this.value, myMap.coordinates))
+        }
+
+        async function placeSearch (category,latLong) {
+            try {
+                const searchParams = new URLSearchParams({
+                  query: category,
+                  ll: latLong,
+                  open_now: 'true',
+                  sort: 'DISTANCE',
+                  limit: 5
+                });
+                const results = await fetch(
+                  `https://api.foursquare.com/v3/places/search?${searchParams}`,
+                  {
+                    method: 'GET',
+                    headers: {
+                      Accept: 'application/json',
+                      Authorization: 'fsq31kaK6M0eHOYrzVjc66oAKgQ7sAg6pwkxvyqH+F3ixUU=',
+                    }
+                  }
+                );
+                const data = await results.json();
+                console.log("data inside the function: " , data)
+                const businesses = data.results // an array of the 5 businesses
+                console.log("business info inside function: ", businesses) 
+                return businesses;
+            } catch (err) {
+                console.error(err);
+            } 
+        }
 
